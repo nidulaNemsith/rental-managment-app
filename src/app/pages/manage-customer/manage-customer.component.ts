784c90eb0e6f0464871bf4d881c11e11e5fadf05
nemsi <nemsithbaduge215@gmail.com>
2024-07-26 17:16:55 +0530

    add ,view customer

--- src/app/pages/manage-customer/manage-customer.component.ts
+++ src/app/pages/manage-customer/manage-customer.component.ts
@@ -1,6 +1,6 @@
 
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-manage-customer',
   standalone: true,
-  imports: [HttpClientXsrfModule,FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './manage-customer.component.html',
   styleUrl: './manage-customer.component.css'
 })
@@ -20,6 +20,8 @@ export class ManageCustomerComponent {
     city:"",
     contact:""
   }
+
+  public customerList:any;
 
   constructor(private http:HttpClient){}
 
@@ -34,8 +36,69 @@ export class ManageCustomerComponent {
         }
       )
 
-
-
+      
   }
+
+  loadTable(){
+    this.http.get("http://localhost:8080/customer-controller/get-all").subscribe(res=>{
+      this.customerList=res;
+      console.log(res);
+    })
+  }
+
+  loadCustomerTable(){
+    this.http.get("http://localhost:8080/customer-controller/get-all").subscribe(res=>{
+      this.customerList=res;
+      console.log(res);
+    })
+  }
+
+  deleteCustomer(id:number){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.http.delete("http://localhost:8080/customer-controller/delete-customer/${id}", { responseType: 'text' }).subscribe(res => {
+          this.loadCustomerTable()
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+          console.log(res);
+
+        })
+        console.log(id);
+
+
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+  }
+      
+  
+  updateCustomer(){}
 
 }
